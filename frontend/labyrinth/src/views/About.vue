24620388afd98b93580ce7d6c74e255882fd3c83
Automatic Linting <data@altamontco.com>
2021-06-20 23:28:07 -0500

    feat: wip - starting connectors

--- frontend/labyrinth/src/views/About.vue
+++ frontend/labyrinth/src/views/About.vue
@@ -1,5 +1,98 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <hr />
+
+    <div class="connector">
+      <!-- Repeated Top connector -->
+      <svg
+        height="50"
+        width="50"
+        style="position: relative; top: 60px; left: 50px"
+      >
+        <line x1="0" y1="0" x2="100" y2="0" stroke="green" stroke-width="20" />
+      </svg>
+
+      <!-- Top turn -->
+      <svg
+        class="angle-connector float-right"
+        width="50"
+        height="50"
+        fill="transparent"
+        stroke-width="10"
+        stroke="green"
+      >
+        <circle cx="50" cy="50" r="35" />
+      </svg>
+
+      <!-- Repeated middle connectors -->
+
+      <svg height="50" width="50" style="margin-left: 10px;">
+        <line x1="0" y1="0" x2="0" y2="100" stroke="green" stroke-width="20" />
+      </svg>
+      <svg height="50" width="50" style="margin-left: 10px;">
+        <line x1="0" y1="0" x2="0" y2="100" stroke="green" stroke-width="20" />
+      </svg>
+
+      <!-- Bottom turn -->
+      <svg
+        class="angle-connector"
+        width="50"
+        fill="transparent"
+        height="50"
+        stroke-width="10"
+        stroke="green"
+      >
+        <circle cx="50" cy="0" r="35" />
+      </svg>
+
+      <!-- Repeated Bottom connector -->
+      <svg
+        height="50"
+        width="50"
+        style="position: relative; left: 50px; bottom: 20px"
+      >
+        <line x1="0" y1="0" x2="100" y2="0" stroke="green" stroke-width="20" />
+      </svg>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {},
+};
+</script>
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+/* SVGs */
+svg {
+}
+.angle-connector {
+}
+
+.connector {
+  width: 100px;
+  display: -ms-inline-grid;
+  display: -moz-inline-grid;
+  display: inline-grid;
+}
+
+/* Color Theme Swatches in Hex */
+.lightblue {
+  color: #83d3df;
+}
+.red {
+  color: #db7077;
+}
+.orange {
+  color: #ffab71;
+}
+.darkblue {
+  color: #375775;
+}
+.superlight {
+  color: #d8faff;
+}
+</style>
