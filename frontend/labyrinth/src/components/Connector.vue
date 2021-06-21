6c2f6031a6269ed57eb21c2cdc11738be910f035
Automatic Linting <data@altamontco.com>
2021-06-20 23:45:31 -0500

    feat: wip - connector

---
+++ frontend/labyrinth/src/components/Connector.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="connector">
+    <!-- Repeated Top connector -->
+    <svg
+      height="50"
+      :width="50 * horizontals"
+      style="position: relative; top: 60px; left: 50px"
+    >
+      <line x1="0" y1="0" :x2="50 * horizontals" y2="0" :stroke="color" stroke-width="20" />
+    </svg>
+
+    <!-- Top turn -->
+    <svg
+      class="angle-connector float-right"
+      width="50"
+      height="50"
+      fill="transparent"
+      stroke-width="10"
+      :stroke="color"
+    >
+      <circle cx="50" cy="50" r="35" />
+    </svg>
+
+    <!-- Repeated middle connectors -->
+
+    <svg height="50" width="50" style="margin-left: 10px" v-for="idx in (1 * verticals)" v-bind:key="idx">
+      <line x1="0" y1="0" x2="0" y2="100" :stroke="color" stroke-width="20" />
+    </svg>
+
+    <!-- Bottom turn -->
+    <svg
+      class="angle-connector"
+      width="50"
+      fill="transparent"
+      height="50"
+      stroke-width="10"
+      :stroke="color"
+    >
+      <circle cx="50" cy="0" r="35" />
+    </svg>
+
+    <!-- Repeated Bottom connector -->
+    <svg
+      height="50"
+      :width="50 * horizontals"
+      style="position: relative; left: 50px; bottom: 20px"
+    >
+      <line x1="0" y1="0" :x2="50 * horizontals " y2="0" :stroke="color" stroke-width="20" />
+    </svg>
+  </div>
+</template>
+<script>
+export default {
+  name: "Connector",
+  props: ["verticals", "horizontals", "color"]
+};
+</script>
+<style scoped>
+/* SVGs */
+svg {
+}
+.angle-connector {
+}
+
+.connector {
+  display: -ms-inline-grid;
+  display: -moz-inline-grid;
+  display: inline-grid;
+  position: relative;
+}
+</style>
--- frontend/labyrinth/src/views/About.vue
+++ frontend/labyrinth/src/views/About.vue
@@ -2,81 +2,49 @@
   <div class="about">
     <hr />
 
-    <div class="connector">
-      <!-- Repeated Top connector -->
-      <svg
-        height="50"
-        width="50"
-        style="position: relative; top: 60px; left: 50px"
-      >
-        <line x1="0" y1="0" x2="100" y2="0" stroke="green" stroke-width="20" />
-      </svg>
+    <Connector
+      verticals="10"
+      horizontals="4"
+      color="green"
+      style="top: 20px; left: 20px"
+      v-if="false"
+    />
+    <Connector verticals="10" horizontals="3" color="orange" v-if="false" />
 
-      <!-- Top turn -->
-      <svg
-        class="angle-connector float-right"
-        width="50"
-        height="50"
-        fill="transparent"
-        stroke-width="10"
-        stroke="green"
-      >
-        <circle cx="50" cy="50" r="35" />
-      </svg>
+    <div class="outer">
+      <div class="left">
+        <div class="corner">
+          <Host />
+        </div>
+        <div class="routes">
+          <Host />
+        </div>
+      </div>
 
-      <!-- Repeated middle connectors -->
-
-      <svg height="50" width="50" style="margin-left: 10px;">
-        <line x1="0" y1="0" x2="0" y2="100" stroke="green" stroke-width="20" />
-      </svg>
-      <svg height="50" width="50" style="margin-left: 10px;">
-        <line x1="0" y1="0" x2="0" y2="100" stroke="green" stroke-width="20" />
-      </svg>
-
-      <!-- Bottom turn -->
-      <svg
-        class="angle-connector"
-        width="50"
-        fill="transparent"
-        height="50"
-        stroke-width="10"
-        stroke="green"
-      >
-        <circle cx="50" cy="0" r="35" />
-      </svg>
-
-      <!-- Repeated Bottom connector -->
-      <svg
-        height="50"
-        width="50"
-        style="position: relative; left: 50px; bottom: 20px"
-      >
-        <line x1="0" y1="0" x2="100" y2="0" stroke="green" stroke-width="20" />
-      </svg>
+      <div class="right">
+        <div class="grouped">
+          <Host />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
+import Connector from "@/components/Connector";
+import Host from "@/components/Host";
 export default {
   data() {},
+  components: {
+    Connector,
+    Host
+  },
 };
 </script>
-<style scoped>
+<style>
 * {
   padding: 0;
   margin: 0;
-}
-/* SVGs */
-svg {
-}
-.angle-connector {
-}
-
-.connector {
-  width: 100px;
-  display: -ms-inline-grid;
-  display: -moz-inline-grid;
-  display: inline-grid;
+  background-color: #fffeff;
 }
 
 /* Color Theme Swatches in Hex */
