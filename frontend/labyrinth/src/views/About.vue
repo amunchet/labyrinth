f60041629669c44a3d084d86835d3449def9d40c
Automatic Linting <data@altamontco.com>
2021-06-21 00:44:01 -0500

    feat(ui): wip - working through the design

--- frontend/labyrinth/src/views/About.vue
+++ frontend/labyrinth/src/views/About.vue
@@ -4,12 +4,11 @@
 
     <Connector
       verticals="10"
-      horizontals="4"
+      horizontals="2"
       color="green"
-      style="top: 20px; left: 20px"
-      v-if="false"
+      style="top: 220px; left: 15px; position: absolute; float: left"
     />
-    <Connector verticals="10" horizontals="3" color="orange" v-if="false" />
+    <Connector verticals="20" horizontals="2" color="orange" style="position:absolute; top:250px; left:40px;" />
 
     <div class="outer">
       <div class="left">
@@ -22,8 +21,57 @@
       </div>
 
       <div class="right">
+        <h2>192.168.0.*</h2>
         <div class="grouped">
+          <h2>Linux</h2>
+          <div>
+            <Host />
+            <Host />
+            <Host />
+            <Host />
+          </div>
+        </div>
+        <div class="grouped">
+          <h2>Windows</h2>
+          <div>
+            <Host />
+            <Host />
+            <Host />
+            <Host />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="outer orange-bg">
+      <div class="left">
+        <div class="corner">
           <Host />
+        </div>
+        <div class="routes">
+          <Host />
+        </div>
+      </div>
+
+      <div class="right">
+        <h2>192.168.0.*</h2>
+        <div class="grouped">
+          <h2>Linux</h2>
+          <div>
+            <Host />
+            <Host />
+            <Host />
+            <Host />
+          </div>
+        </div>
+        <div class="grouped">
+          <h2>Windows</h2>
+          <div>
+            <Host />
+            <Host />
+            <Host />
+            <Host />
+          </div>
         </div>
       </div>
     </div>
@@ -36,15 +84,61 @@ export default {
   data() {},
   components: {
     Connector,
-    Host
+    Host,
   },
 };
 </script>
 <style>
-* {
+body,
+html {
   padding: 0;
   margin: 0;
   background-color: #fffeff;
+}
+
+h2 {
+}
+
+.outer {
+  background-color: #efefed;
+  min-height: 300px;
+  overflow: hidden;
+  margin: auto;
+  margin-left: 100px;
+  margin-right: 1%;
+  margin-top: 20px;
+  border-radius: 0.25rem;
+}
+
+.left {
+  width: 25%;
+  min-width: 150px;
+  float: left;
+  display: flex;
+  min-height: 300px;
+}
+.corner {
+  position: relative;
+  top: 0;
+  left: 0;
+  border-radius: 0.25rem;
+  background-color: #375775;
+  color: white;
+  min-width: 150px;
+  height: 250px;
+  border-right: 4px solid lightgrey;
+  border-bottom: 4px solid lightgrey;
+}
+.routes {
+  width: 100%;
+}
+.grouped {
+  border: 1px solid green;
+  float: right;
+  min-width: 300px;
+}
+.grouped div {
+  display: inline-flex;
 }
 
 /* Color Theme Swatches in Hex */
@@ -62,5 +156,21 @@ export default {
 }
 .superlight {
   color: #d8faff;
+}
+
+.lightblue-bg{
+  background-color: #83d3df;
+}
+.red-bg{
+  background-color: #db7077;
+}
+.orange-bg{
+  background-color: #ffab71;
+}
+.darkblue-bg{
+  background-color: #375775;
+}
+.superlight-bg{
+  background-color: #d8faff;
 }
 </style>
