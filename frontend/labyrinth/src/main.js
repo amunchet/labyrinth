a2c9dcbb283e9d32e2a4cd3d7e023a0b3dbe73f9
Automatic Linting <data@altamontco.com>
2021-06-25 01:00:07 -0500

    feat(js): added the new js files

--- frontend/labyrinth/src/main.js
+++ frontend/labyrinth/src/main.js
@@ -1,19 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
-
-
-//Auth
-import AuthPlugin from './plugins/auth'
-
-import router from './router'
-
-// Install the authentication plugin here
-Vue.use(AuthPlugin)
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).use(store).use(router).mount("#app");
