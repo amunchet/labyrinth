184036b322400551fc39f9d0c3e2242eacc2bf20
Chester Enright <cenright@altamontco.com>
2021-07-07 11:18:26 -0500

    feat(ui): light logo and starting on top bar

--- frontend/labyrinth/src/App.vue
+++ frontend/labyrinth/src/App.vue
@@ -1,9 +1,59 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <b-navbar toggleable="lg" type="light" class="pb-0 pt-0">
+        <b-navbar-brand href="#" class="top_logo">
+          <img src="/logo.png" alt="Labyrinth logo" />
+        </b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav class="pt-3">
+          <b-navbar-nav>
+            <b-nav-item href="#">
+              <router-link to="/">
+                <font-awesome-icon icon="home" size="1x" />
+              </router-link>
+            </b-nav-item>
+            <b-nav-item href="/about">
+              <router-link to="/about">Dashboard</router-link>
+            </b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input
+                size="sm"
+                class="mr-sm-2"
+                placeholder="Search"
+              ></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                >Search</b-button
+              >
+            </b-nav-form>
+
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
+    <hr class="m-2" />
+    <br />
     <router-view />
   </div>
 </template>
@@ -28,5 +78,16 @@
       color: #42b983;
     }
   }
+}
+.top_logo {
+  position: absolute;
+  left:20px;
+  top: 0;
+}
+.top_logo img {
+  height: 75px;
+}
+#nav-collapse{
+  margin-left: 75px;
 }
 </style>
