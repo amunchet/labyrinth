fb1567bbee1191f651d371a989734b144adf0b5f
Chester Enright <cenright@altamontco.com>
2021-07-08 15:28:03 -0500

    test: wip - starting on service tests

--- backend/serve.py
+++ backend/serve.py
@@ -6,21 +6,36 @@ Sample Flask app
 import functools
 import os
 
+import pymongo
+
 from common import auth
 from flask import Flask, request
 from flask_cors import CORS
+
 
 PERM_READ = "read"
 PERM_WRITE = "write"
 PERM_ADMIN = "reports"
 
 
-requires_auth_read = functools.partial(auth._requires_auth, permission=PERM_READ)
-requires_auth_write = functools.partial(auth._requires_auth, permission=PERM_WRITE)
-requires_auth_admin = functools.partial(auth._requires_auth, permission=PERM_ADMIN)
+requires_auth_read = functools.partial(
+    auth._requires_auth, permission=PERM_READ)
+requires_auth_write = functools.partial(
+    auth._requires_auth, permission=PERM_WRITE)
+requires_auth_admin = functools.partial(
+    auth._requires_auth, permission=PERM_ADMIN)
 
 app = Flask(__name__)
 CORS(app)
+
+# Mongo Access
+mongo_client = pymongo.MongoClient(
+    "mongodb://{}:{}@{}:27017".format(
+        os.environ.get("MONGO_USERNAME"),
+        os.environ.get("MONGO_PASSWORD"),
+        os.environ.get("MONGO_HOST"),
+    )
+)
 
 
 # Route definitions
@@ -29,13 +44,14 @@ CORS(app)
 def insecure():
     return "Insecure route.", 200
 
+
 @app.route("/secure")
 @requires_auth_read
 def secure():
     return "Secure route.", 200
 
 
-if __name__ == "__main__": # Run the Flask server in development mode
+if __name__ == "__main__":  # Run the Flask server in development mode
     app.debug = True
     app.config["ENV"] = "development"
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7000)))
--- backend/test/test_serve.py
+++ backend/test/test_serve.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import requests
+import pytest
+import serve
+
+from common.test import unwrap
 
 
 def test_insecure():
@@ -19,22 +23,147 @@ def test_secure():
 
 # Labyrinth main functions
 
+def teardown():
+    """Tears down tests"""
+    serve.mongo_client["labyrinth"]["subnets"].delete_many({})
+    serve.mongo_client["labyrinth"]["groups"].delete_many({})
+    serve.mongo_client["labyrinth"]["hosts"].delete_many({})
+    serve.mongo_client["labyrinth"]["services"].delete_many({})
+    serve.mongo_client["labyrinth"]["settings"].delete_many({})
+    
+
+@pytest.fixture
+def setup():
+    """Sets up tests"""
+    teardown()
+    yield "Setting up..."
+    teardown()
+    return "Done"
+
+
+def test_create_edit_subnet(setup):
+    """
+    Creates/Edits a subnet in the database
+        - (_id)
+        - Entry point (can be 127.0.0.1)
+        - Link to other subnet
+    """
+    sample_subnet = {
+        "subnet" : "192.168.0",
+        "origin" : {
+            "ip" : "127.0.0.1",
+            "icon" : "VMWare"
+        },
+        "links" : {
+            "ref" : "start_1",
+            "ip" : ".175",
+            "icon" : "Router",
+            "color": "orange"
+        }
+    }
+
+    # Create it
+    a = unwrap(serve.create_edit_subnet)(sample_subnet)
+    assert a[1] == 200
+
+    b = serve.mongo_client["labyrinth"]["subnets"].find({})
+    c = [x for x in b]
+    assert len(c) == 1
+
+    assert c[0]["subnet"] == "192.168.0"
+    assert c[0]["links"] == sample_subnet["links"]
+
+    # Edit it
+
+    sample_subnet["links"]["ref"] = "start_2"
+    a = unwrap(serve.create_edit_subnet)(sample_subnet)
+    assert a[1] == 200
+
+    b = serve.mongo_client["labyrinth"]["subnets"].find({})
+    c = [x for x in b]
+    assert len(c) == 1
+
+    assert c[0]["subnet"] == "192.168.0"
+    assert c[0]["groups"] == sample_subnet["groups"]
+    assert c[0]["links"] != sample_subnet["links"]
+    assert c[0]["links"]["ref"] == "start_2"
+
+def test_create_group(setup):
+    """
+    Creates/Edits Group
+        - (_id)
+        - Name
+        - Children MACs
+
+    """
+    sample_group = {
+        "name" : "Linux Servers",
+        "members": ["00-00-00-01", "00-00-00-02"]
+    }
+    a = unwrap(serve.create_edit_group)(sample_group)
+    assert a[1] == 200
+
+    b = serve.mongo_client["labyrinth"]["groups"].find({})
+    c = [x for x in b]
+    assert len(c) == 1
+
+    assert c[0]["name"] == sample_group["name"]
+    assert c[0]["members"] == sample_group["members"]
+
+def test_create_edit_host(setup):
+    """
+    Creates/Edits a host for the given subnet
+        - (_id)
+        - IP address
+        - Subnet
+        - MAC Address
+        - Icon
+        - Services
+        - Class: this determines if we are monitoring standard vitals (PC) or not (phone)
+
+    """
+    sample_host = {
+        "ip" : "192.168.0.172",
+        "subnet" : "192.168.0",
+        "mac" : "00-00-00-00-01",
+        "group" : "Linux Servers",
+        "icon" : "linux",
+        "services": [
+            "port_ssh",
+            "metric_cpu",
+            "metric_hd",
+            "metric_ram"
+        ],
+        "class" : "health"
+    }
+
+    # Create Host - what are the implications for the group and subnet?
+    a = unwrap(serve.create_edit_host)(sample_host)
+    assert a[1] == 200
+
+    b = serve.mongo_client["labyrinth"]["hosts"]
+    b = serve.mongo_client["labyrinth"]["groups"]
+    b = serve.mongo_client["labyrinth"]["subnets"]
 
 
 
-def test_create_edit_subnet():
-    """Creates/Edits a subnet in the database"""
+    # Edit Host - what are the implications for the group and subnet?
 
 
-def test_create_edit_host():
-    """Creates/Edits a host for the given subnet"""
-
-
-
-def test_create_edit_link():
+def test_create_edit_link(setup):
     """Creates/Edits a link between two subnets"""
     
-def test_list_dashboard():
+
+def test_delete_subnet(setup):
+    """Deletes a subnet"""
+
+def test_delete_group(setup):
+    """Deletes a Group"""
+
+def test_delete_host(setup):
+    """Deletes a Host"""
+
+def test_list_dashboard(setup):
     """
     Lists all items for the dashboard
         - Subnets
@@ -52,7 +181,7 @@ Tests for Services
     - For example, a service could be reading the metric of "mem" for a host and seeing if "free" was greater than 1000
 """
 
-def test_create_service():
+def test_create_service(setup):
     """
     Creating services is difficult.  
         - Need to have the JSON definiton
@@ -60,5 +189,10 @@ def test_create_service():
         - Need to have the boolean comparison operations - and, not, or, contains, etc.
     """
 
-def test_read_services():
+def test_read_services(setup):
     """Returns the services for a given host"""
+
+def test_delete_service(setup):
+    """Deletes a Service"""
+
+    # Implications for hosts that have this service enabled
