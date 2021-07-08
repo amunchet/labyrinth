d778d4f435965947d5f73456097d6468dc2b3632
Chester Enright <cenright@altamontco.com>
2021-07-08 15:27:50 -0500

    chore: added nmap to requirements and run_tests

---
+++ backend/run_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+CODECOV="95"
+DOCKER_PROD_NAME="labyrinth_backend_1"
+
+# Running Python unit tests and coverage
+echo "Running Pytest..."
+# -x is stop on first failure
+
+
+echo "Running tests in local"
+docker exec $DOCKER_PROD_NAME sh -c "cd /src && PYTHONPATH=/src pytest --cov=/src --cov-report term-missing --cov-fail-under=95 -x ."
