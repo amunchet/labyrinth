3127a6a9f44aab92c097c0bfb4a2869a69a5c218
Chester Enright <cenright@altamontco.com>
2021-07-14 18:55:39 -0500

    feat: wip - started on vulns functions

---
+++ vulns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Vulns work
+    - Going to need to talk to the new docker setup.
+    - Also update.
+    - Needs to show the list of latest vulnerabilities, so we know update is working properly.
+
+https://cve.circl.lu/api/
+
+`curl .../api/cvefor/[CPE Identifier from NMAP]`
+"""
