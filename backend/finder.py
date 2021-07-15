0651aace8950aee9d90edee4da45df1f166261b1
Chester Enright <cenright@altamontco.com>
2021-07-15 17:03:49 -0500

    feat: wip - running nmap and output to settings view

--- backend/finder.py
+++ backend/finder.py
@@ -3,6 +3,7 @@
 Auto discovery finder
 """
 import time
+import json
 
 import redis
 
@@ -11,20 +12,26 @@ from serve import list_subnets
 
 from pid import PidFile
 from typing import Dict, List
-from nmap import PortScanner as ps
+from nmap import PortScannerYield as ps
 
 from common.test import unwrap
 from serve import list_subnet, list_subnets, create_edit_host 
 
-def scan(subnet: str) -> List: 
+def scan(subnet: str, callback_fn) -> List: 
     """Scans a given subnet"""
     search = subnet
     if len(subnet.split(".")) == 3:
         search += ".0/24"
 
     scanner = ps()
-    scanner.scan(search)
-    return[scanner[host] for host in scanner.all_hosts()]
+    results = []
+    for line in scanner.scan(search):
+        if line[1]["nmap"]["scanstats"]["uphosts"] != '0':
+            callback_fn("\n" + str(line[0]) + ": " + str(line[1]["nmap"]["scanstats"]) + "\n")
+            results.append(line[1])
+        else:
+            callback_fn(".")
+    return results
 
 def convert_host(input: Dict) -> Dict:
     """
@@ -76,21 +83,28 @@ def process_scan(input: Dict) -> Dict:
 
 def main():
     """Runs scan and updates database"""
+
+    rclient = redis.Redis(host="redis")
+    def update_redis(msg):
+        output = rclient.get("output").decode("utf-8")
+        rclient.set("output", output + str(msg))
+
     with PidFile("labyrinth-finder") as p:
-        rclient = redis.Redis(host="redis")
         rclient.set("output", "")
 
         # List each subnet
-        subnets = unwrap(serve.list_subnets)()[0]
-
-        # Lists hosts in database with subnet
-
-        # Scan - at each line, update redis
-
-        # For each host, if it doesn't exist, create
+        subnets = json.loads(unwrap(list_subnets)()[0])
         
-        # Insert metric for the host
+        for subnet in subnets:
+            update_redis("\nStarting {}".format(subnet))
+            results = scan(subnet, update_redis)
+
+        # TODO: For each host, if it doesn't exist, create it.
+
+        # TODO: Maybe guess on the type (lowercase of first word of OS match?)
+        
+        # TODO: Insert metric for the host
+        update_redis("Finished.\n")
 
 if __name__ == "__main__":
-    # Main run loop
-    pass
+    main()
--- frontend/labyrinth/src/views/Settings.vue
+++ frontend/labyrinth/src/views/Settings.vue
@@ -1,65 +1,76 @@
 <template>
-    <b-container>
-        <h2>Settings</h2>
-        <hr />
-        <b-row>
-            <b-col>
-                Enter Subnet to scan: <br />
-                (i.e. 192.168.0.0/24)
-            </b-col>
-            <b-col>
-                <b-input v-model="subnet" />
-            </b-col>
-            <b-col class="text-left">
-                <b-button variant="primary" @click="startScan()">Start</b-button>
-            </b-col>
-        </b-row>
-        <hr />
-        <b-textarea disabled v-model="data" />
-    </b-container>
-    
+  <b-container>
+    <h3>Settings</h3>
+    <hr />
+    <b-row>
+      <b-col>
+        Enter Subnet to scan: <br />
+        (i.e. 192.168.0.0/24)
+      </b-col>
+      <b-col>
+        <b-input v-model="subnet" />
+      </b-col>
+      <b-col class="text-left">
+        <b-button variant="primary" @click="startScan()">Start</b-button>
+      </b-col>
+    </b-row>
+    <hr />
+    <b-textarea ref="textarea_1" disabled v-model="data" />
+  </b-container>
 </template>
 <script>
-import Helper from '@/helper'
+import Helper from "@/helper";
 export default {
-    name: "Settings",
-    data(){
-        return{
-        data: "",
-        subnet: "",
+  name: "Settings",
+  data() {
+    return {
+      data: "",
+      subnet: "",
+    };
+  },
+  methods: {
+    loadData: /* istanbul ignore next */ function () {
+      var auth = this.$auth;
+      Helper.apiCall("redis", "", auth).then((res) => {
+        this.data = res;
+        this.$forceUpdate();
+        if (res.indexOf("Finished") == -1) {
+          var el = this.$refs.textarea_1.$el;
+          el.scrollTop = el.scrollHeight + 500;
+          this.$forceUpdate();
+          el = this.$refs.textarea_1.$el;
+          el.scrollTop = el.scrollHeight + 500;
+          this.$forceUpdate();
         }
+      });
     },
-    methods: {
-        loadData: /* istanbul ignore next */ function(){
-            var auth = this.$auth
-            Helper.apiCall("redis", "", auth).then(res=>{
-                this.data = res
-            }).catch(e=>{
-                this.$store.commit('updateError', e)
-            })
-        },
-        startScan: /* istanbul ignore next */ function(){
-            var auth = this.$auth
-            var formData = new FormData()
-            formData.append("data", this.subnet)
-            Helper.apiPost("scan", "", "", auth, formData).then(res=>{
-                this.$store.commit('updateError', res)
-            }).catch(e=>{
-                this.$store.commit('updateError', e)
-            })
-        },
+    startScan: /* istanbul ignore next */ function () {
+      var auth = this.$auth;
+      var formData = new FormData();
+      formData.append("data", this.subnet);
+      Helper.apiPost("scan", "", "", auth, formData)
+        .then((res) => {
+          this.$store.commit("updateError", res);
+        })
+        .catch((e) => {
+          this.$store.commit("updateError", e);
+        });
     },
-    mounted: function(){
-        try{
-            setInterval(this.loadData, 2000)
-        }catch(e){
-            this.$store.commit('updateError', e)
-        }
+  },
+  mounted: function () {
+    try {
+      setInterval(this.loadData, 2000);
+    } catch (e) {
+      this.$store.commit("updateError", e);
     }
-}
+  },
+};
 </script>
 <style scoped>
-    textarea{
-        min-height: 400px;
-    }
+textarea {
+  height: 400px;
+  padding: 0;
+  padding-left: 1rem;
+  padding-bottom: -40px;
+}
 </style>
