57920f3e66acee2b7e67d597aad55050ec15caac
Chester Enright <cenright@altamontco.com>
2021-07-20 17:03:22 -0500

    test: added services tests

--- backend/test/test_services.py
+++ backend/test/test_services.py
@@ -2,10 +2,16 @@
 """
 Tests for the TOML work
 """
+import json
+
+import redis
 import pytest
 
 import services
+import serve
 import toml
+
+from common.test import unwrap
 
 @pytest.fixture
 def setup():
@@ -25,35 +31,58 @@ def setup():
     # TODO: Multiline Arrays
     yield (x, decoder)
 
+def test_generate_structure(setup):
+    """
+    Tests generating an object from the TOML
+    """
+    lines,decoder = setup
+    x = services.parse(lines["inputs"]["postgresql_extensible"][0])
+
+    assert x["address"] == "host=localhost user=postgres sslmode=disable"
+    assert x["query"] == [{'measurement': '', 'sqlquery': 'SELECT * FROM pg_stat_bgwriter', 'version': 901, 'withdbname': False, 'tagvalue': 'postgresql.stats'}]
+
 # Redis
 
 
-def test_put_comments():
+def test_redis_comments():
     """
     Store all comments into Redis
         - Renamed - put the parent + . + name split before = and stripped
         - Call these by their keys in `decoder.before_tags`
 
     """
+    a = unwrap(serve.put_comments)()
+    assert a[1] == 200
 
-def test_put_structure():
+    name = "inputs.sysstat.device_tags.sda"
+    b = unwrap(serve.get_comment)(name)
+    assert b[1] == 200
+    
+    assert json.loads(b[0])["comments"] == ['Currently supported formats:']
+
+    name = "inputs.execd.command"
+    b = unwrap(serve.get_comment)(name)
+    assert b[1] == 200
+    
+    assert json.loads(b[0])["comments"] ==  ['- "EVENTHUB_NAME"']
+
+
+
+def test_redis_structure():
     """
     Push JSON of the actual structure to Redis
         - Called "master.data"
 
     """
+    a = services.generate_structure()
+    
+    b = unwrap(serve.put_structure)()
+    assert b[1] == 200
 
-# Get data
+    c = unwrap(serve.get_structure)()
+    assert c[1] == 200
+    assert a == c[0]
 
-def test_get_structure():
-    """
-    Gets stored Redis structure
-    """
-
-def test_get_comment():
-    """
-    Gets comment
-    """
 
 # Compile
 
@@ -61,8 +90,9 @@ def compile_snippets():
     """
     Compile selected snippets into working TOML files
     """
-
+    assert False
 def test_telegraf_conf():
     """
     Tests that the resultant conf is valid for telegraf
     """
+    assert False
