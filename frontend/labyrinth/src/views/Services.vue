801bcb0449867d2e42352426e55c81f9ba7d4437
Chester Enright <cenright@altamontco.com>
2021-07-20 16:34:42 -0500

    feat: wip - starting on services page

---
+++ frontend/labyrinth/src/views/Services.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-container>Services
+  <b-row v-for="(section, idx) in data" v-bind:key="idx">
+    <h2>{{section.name}}</h2>
+    <b-select :options="list_children" />
+    <b-button>Add Section</b-button>
+
+    {{details}}
+    <hr />
+  </b-row>
+</b-container>
+</template>
+
+<script>
+export default {
+  name: "Services",
+  data() {
+    return {
+      data: [
+        {
+          name: "global_tags",
+        },
+        {
+          name: "inputs",
+
+        },
+        {
+          name: "agent",
+        },
+      ],
+
+      // List children for a specific element
+      list_children: ['cpu', 'disk', 'diskio', 'kernel', 'mem'],
+      
+      // Details - this will look for parent of whatever
+      details: {
+        name: "cpu",
+        comment: "",
+        fields: {
+          "percpu" : {
+            "raw" : "percpu = true",
+            "comments" : [
+              "Read metrics about cpu usage"
+            ]
+          },
+          "totalcpu" : "",
+          "collect_cpu_time": "",
+          "report_active": "",
+        }
+      },
+    };
+  },
+};
+</script>
+
+<style scoped></style>
