71b72ef20846fd3e46bf23525b597eac2334f9c3
Chester Enright <cenright@altamontco.com>
2021-07-22 19:24:10 -0500

    feat(ui): wip - updates to deploy

--- frontend/labyrinth/src/views/Deploy.vue
+++ frontend/labyrinth/src/views/Deploy.vue
@@ -2,56 +2,209 @@
   <b-container>
     <b-row>
       <b-col>
+        <h4>Deployment Files</h4>
+        All of these must be encrypted vault files.
+        <hr />
+
         <b-row>
-          <h4>Files</h4> </b-row
-        ><b-row>
-          <ul class="text-left">
-            <li>SSH Key</li>
-            <li>TOTP Key</li>
-            <li>Become Password</li>
-            <li>Telegraf conf file</li>
-            <li>Ansible Files (optional)</li>
-          </ul>
-        </b-row>
-        <b-row>
-          <b-button>Upload</b-button>
-        </b-row>
-        <b-row>
-          Telegraf deployment is interesting, because there are some non x64 machines here.
-          FreeBSD, old arm processors, etc.
-        </b-row>
-              </b-col>
-      <b-col>
-        <h2>Deploy</h2>
-        <b-row>
-          Host: <b-select />
-        </b-row>
-        <b-row>
+          <b-col> SSH Key </b-col>
           <b-col>
-          <h5>Ansible playbook</h5>
-          Validating playbook...
-          <textarea /><br />
-
-<b-row>
-          Password: 
-            <b-input type="password" />
-        </b-row>
-
-          <b-button variant="success">Deploy</b-button>
-
-          Test deployment in docker...
+            <b-select :options="ssh_key_files" />
+          </b-col>
+          <b-col cols="4">
+            <b-form-file
+              class="float-left"
+              v-model="ssh_key_file"
+              placeholder="..."
+              drop-placeholder="Drop here..."
+            ></b-form-file> </b-col
+          ><b-col cols="1">
+            <b-button variant="link" class="m-0 mt-2 p-0 float-left">
+              <font-awesome-icon icon="times" size="1x" />
+            </b-button>
           </b-col>
         </b-row>
-        </b-col>
+        <b-row>
+          <b-col> TOTP Key </b-col>
+          <b-col>
+            <b-select />
+          </b-col>
+          <b-col cols="4">
+            <b-form-file
+              v-model="file1"
+              placeholder="..."
+              drop-placeholder="Drop here..."
+            ></b-form-file>
+          </b-col>
+          <b-col cols="1">
+            <b-button variant="link" class="m-0 mt-2 p-0 float-left">
+              <font-awesome-icon icon="times" size="1x" />
+            </b-button>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col> Become Password </b-col>
+          <b-col>
+            <b-select />
+          </b-col>
+          <b-col cols="4">
+            <b-form-file
+              v-model="file1"
+              placeholder="..."
+              drop-placeholder="Drop here..."
+            ></b-form-file>
+          </b-col>
+          <b-col cols="1">
+            <b-button variant="link" class="m-0 mt-2 p-0 float-left">
+              <font-awesome-icon icon="times" size="1x" />
+            </b-button>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col> Telegraf conf file </b-col>
+          <b-col>
+            <b-select />
+          </b-col>
+          <b-col cols="4">
+            <b-form-file
+              v-model="file1"
+              placeholder="..."
+              drop-placeholder="Drop here..."
+            ></b-form-file>
+          </b-col>
+          <b-col cols="1">
+            <b-button variant="link" class="m-0 mt-2 p-0 float-left">
+              <font-awesome-icon icon="times" size="1x" />
+            </b-button>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col> Ansible Files (optional) </b-col>
+          <b-col>
+            <b-select />
+          </b-col>
+          <b-col cols="4">
+            <b-form-file
+              v-model="file1"
+              placeholder="..."
+              drop-placeholder="Drop here..."
+            ></b-form-file>
+          </b-col>
+          <b-col cols="1">
+            <b-button variant="link" class="m-0 mt-2 p-0 float-left">
+              <font-awesome-icon icon="times" size="1x" />
+            </b-button>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col> Other Ansible Files (optional) </b-col>
+          <b-col>
+            <b-select />
+          </b-col>
+          <b-col cols="4">
+            <b-form-file
+              v-model="file1"
+              placeholder="..."
+              drop-placeholder="Drop here..."
+            ></b-form-file>
+          </b-col>
+          <b-col cols="1">
+            <b-button variant="link" class="m-0 mt-2 p-0 float-left">
+              <font-awesome-icon icon="times" size="1x" />
+            </b-button>
+          </b-col>
+        </b-row>
+        <hr />
+        <div class="text-left">
+          Telegraf deployment is interesting, because there are some non x64
+          machines here. FreeBSD, old arm processors, etc.
+        </div>
+        <hr />
+        <div class="text-left">
+          To create ansible vault files, use the following command: 
+          ```
+          ansible-vault create [FILENAME]
+          ```
+          New Vault Password: XXXXXXX
+          
+          [Edit file]
+        </div>
+      </b-col>
+      <b-col>
+        <h2>Deploy</h2>
+        Host: <b-select />
+        <h5 class="mt-2 mb-2">Ansible playbook</h5>
+        Validating playbook...
+        <textarea /><br />
+
+        Password:
+        <b-input type="password" />
+        <hr />
+        <b-button variant="success">Deploy</b-button>
+        <br />
+        Test deployment in docker...
+      </b-col>
     </b-row>
   </b-container>
 </template>
 <script>
-export default {};
+import Helper from "@/helper";
+export default {
+  name: "Deploy",
+  watch: {
+    ssh_key_file: function (val) {
+      if (val != "") {
+        var auth = this.$auth;
+        var formData = new FormData();
+        formData.append("file", this.ssh_key_file);
+        Helper.apiPost("upload", "/ssh", auth.accessToken, auth, formData, true)
+          .then((res) => {
+            this.$store.commit("updateError", res);
+          })
+          .catch((e) => {
+            this.$store.commit("updateError", e);
+          });
+      }
+    },
+  },
+  methods: {
+    load_ssh_keys: /* istanbul ignore next */ function () {
+      var auth = this.$auth;
+      Helper.apiCall("uploads", "ssh", auth)
+        .then((res) => {
+          this.ssh_key_files = res;
+        })
+        .catch((e) => {
+          this.$store.commit("updateError", e);
+        });
+    },
+  },
+  data() {
+    return {
+      ssh_key_file: "",
+      ssh_key_files: [],
+    };
+  },
+  mounted: /* istanbul ignore next */ function () {
+    try {
+      this.load_ssh_keys();
+    } catch (e) {
+      this.$store.commit("updateError", e);
+    }
+  },
+};
 </script>
 <style lang="scss" scoped>
-textarea{
+textarea {
   width: 100%;
   min-height: 400px;
+}
+.row {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+.col {
+  text-align: left;
+  margin-left: 2rem;
 }
 </style>
