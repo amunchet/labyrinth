bbe9b0c525c3b16a7ec259276e4931c9bf9ad268
Chester Enright <amunchet@gmail.com>
2021-07-24 19:44:09 -0500

    feat: wip - getting selections for metrics conf file working

--- frontend/labyrinth/src/components/Service.vue
+++ frontend/labyrinth/src/components/Service.vue
@@ -1,21 +1,38 @@
 <template>
-  <div :class="determineClass()" :id="name" v-if="(isParent || service_filter == '' || (parent + name).indexOf(service_filter) != -1) && (name != '' && typeof data == 'object' || !minimized)"> 
+  <div
+    :class="determineClass()"
+    :id="name"
+    v-if="
+      (isParent ||
+        service_filter == '' ||
+        (parent + name).indexOf(service_filter) != -1) &&
+      ((name != '' && typeof data == 'object') || !minimized)
+    "
+  >
     <h2 v-if="name != '' && typeof data == 'object'">
-      <b-button variant="link" class="mt-0 pt-0 pb-0 mb-0" @click="minimized = !minimized">
-        <font-awesome-icon v-if="minimized" icon="caret-up" size="2x" />
-        <font-awesome-icon v-if="!minimized" icon="caret-down" size="2x" />
+      <b-button
+        variant="link"
+        class="mt-0 pt-0 pb-0 mb-0 pr-1 shadow-none"
+        @click="minimized = !minimized"
+      >
+        <font-awesome-icon class="pt-1" v-if="minimized" icon="caret-right" size="2x" />
+        <font-awesome-icon class="pt-1"  v-if="!minimized" icon="caret-down" size="2x" />
       </b-button>
-      {{ format(name) }} 
-      <b-button class="float-right" variant="link" @click="loadComment()">
+      {{ format(name) }}
+      <b-button v-if="depth *1 < 2" class="mb-0 mt-1 p-0 pl-1 pr-1 mr-1 float-right" variant="success" @click="add(data, name, parent)">
+        <font-awesome-icon class="pt-1" icon="plus" size="1x" />
+      </b-button>
+
+      <b-button class="float-right shadow-none" variant="link" @click="loadComment()">
         <font-awesome-icon icon="info-circle" size="1x" />
       </b-button>
     </h2>
-    <div >
-    <div class="text-left comment" v-if="comment.comments != undefined">
-      <span v-for="(comment, i) in comment.comments" v-bind:key="i">
-        {{ comment.replace(/#/g, "") }}&nbsp;
-      </span>
-    </div>
+    <div>
+      <div class="text-left comment" v-if="comment.comments != undefined">
+        <span v-for="(comment, i) in comment.comments" v-bind:key="i">
+          {{ comment.replace(/#/g, "") }}&nbsp;
+        </span>
+      </div>
 
       <div v-if="typeof data == 'object' && Array.isArray(data)">
         <div class="overflow-hidden">
@@ -32,6 +49,8 @@
           :parent="parent + '.' + name"
           :start_minimized="minimized"
           service_filter=""
+          :depth="depth * 1 + 1"
+          @add="parsed"
         />
       </div>
       <div v-else-if="typeof data == 'object'">
@@ -43,6 +62,8 @@
           :parent="parent + '.' + name"
           :start_minimized="minimized"
           :service_filter="service_filter"
+          :depth="depth * 1 + 1"
+          @add="parsed"
         />
       </div>
       <div v-else-if="typeof data == 'number'">
@@ -61,7 +82,7 @@
           </b-col>
         </b-row>
       </div>
-      <div v-else-if="typeof data == 'boolean' ">
+      <div v-else-if="typeof data == 'boolean'">
         <b-row>
           <b-col>
             <div v-if="!arrayChild" class="float-left mt-1 text-capitalize">
@@ -92,13 +113,22 @@
         </b-row>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 <script>
 import Helper from "@/helper";
 export default {
   name: "ServiceComponent",
-  props: ["name", "data", "arrayChild", "parent", "start_minimized", "isParent", "service_filter"],
+  props: [
+    "name",
+    "data",
+    "arrayChild",
+    "parent",
+    "start_minimized",
+    "isParent",
+    "service_filter",
+    "depth"
+  ],
   data() {
     return {
       comment_name: "",
@@ -108,6 +138,21 @@ export default {
     };
   },
   methods: {
+    add: function(item, name, parent){
+
+      var structure = {
+        "item" : item,
+        "name" : name,
+        "parent" : parent || ""
+      }
+
+      this.$emit("add", JSON.stringify(structure))
+      this.$forceUpdate()
+    },
+    parsed: function(item){
+      var structure = JSON.parse(item)
+      this.add(structure.item, structure.name, structure.parent)
+    },
     loadComment: /* istanbul ignore next */ function () {
       if (this.comment != "") {
         this.comment = "";
@@ -125,8 +170,8 @@ export default {
     determineClass: function () {
       if (typeof this.data == "object" && Array.isArray(this.data)) {
         return "main array child";
-      } else if(typeof this.data != "object"){
-        return "child"
+      } else if (typeof this.data != "object") {
+        return "child";
       } else {
         return "main border";
       }
@@ -139,21 +184,19 @@ export default {
       }
     },
   },
-  watch:{
-    start_minimized: function(val){
-      this.minimized = val
+  watch: {
+    start_minimized: function (val) {
+      this.minimized = val;
     },
-    service_filter: function(val){
-      if(val != ""){
-        this.minimized = false
-      }else{
-        this.minimized = true
+    service_filter: function (val) {
+      if (val != "") {
+        this.minimized = false;
+      } else {
+        this.minimized = true;
       }
-    }
+    },
   },
   mounted: function () {
-
-
     if (this.parent != undefined) {
       this.comment_name = (this.parent + "." + this.name)
         .replace(".0", "")
@@ -162,26 +205,25 @@ export default {
       this.comment_name = this.name;
     }
 
-    if (this.start_minimized != undefined){
-      this.minimized = this.start_minimized
+    if (this.start_minimized != undefined) {
+      this.minimized = this.start_minimized;
     }
   },
 };
 </script>
 <style lang="scss" scoped>
-
 // Specific styles
-#outputs{
+#outputs {
   max-height: 400px;
   overflow-y: scroll;
 }
 
-#processors{
+#processors {
   max-height: 400px;
   overflow-y: scroll;
 }
 
-#inputs{
+#inputs {
   max-height: 400px;
   overflow-y: scroll;
 }
@@ -189,7 +231,7 @@ export default {
 .comment:first-letter {
   text-transform: capitalize;
 }
-.comment{
+.comment {
   padding: 1rem;
   margin: 1rem;
   background-color: #efefed;
@@ -202,7 +244,7 @@ h2 {
   line-height: 1;
   margin: 0.25rem;
 }
-.child h2{
+.child h2 {
   font-size: 12pt;
 }
 .border {
@@ -211,16 +253,15 @@ h2 {
 }
 .main {
   margin: 0.5rem;
-
 }
-.array{
+.array {
   margin-bottom: 1rem;
 }
-.child{
+.child {
   margin: 0.5rem;
   border-bottom: 1px solid #efefed;
 }
-.row{
+.row {
   margin-top: 0.25rem;
   padding-left: 1rem;
   padding-right: 0.5rem;
--- frontend/labyrinth/src/views/Services.vue
+++ frontend/labyrinth/src/views/Services.vue
@@ -10,19 +10,20 @@
             v-model="temp_filter"
             @blur="service_filter = temp_filter"
           ></b-form-input>
-          
         </b-row>
         <hr />
         <div v-if="loaded">
-        <ServiceComponent
-          v-for="(section, idx) in data"
-          v-bind:key="idx"
-          :name="idx"
-          :data="section"
-          :start_minimized="true"
-          :isParent="true"
-          :service_filter="service_filter"
-        />
+          <ServiceComponent
+            v-for="(section, idx) in data"
+            v-bind:key="idx"
+            :name="idx"
+            :data="section"
+            :start_minimized="true"
+            :isParent="true"
+            :service_filter="service_filter"
+            @add="add"
+            depth="0"
+          />
         </div>
         <b-spinner class="m-2" v-else />
       </b-col>
@@ -31,9 +32,20 @@
         <b-select />
         <b-button class="m-2">Save</b-button>
         <hr />
-        <h4>Test Configuration File</h4>
         <br />
-        <textarea />
+        <div 
+          v-if="output_data"
+          >
+        <ServiceComponent
+          v-for="(section, idx) in output_data"
+          v-bind:key="idx"
+          :name="idx"
+          :data="section"
+          :start_minimized="true"
+          :isParent="true"
+          depth="0"
+        />
+        </div>
 
         <b-button class="m-2 mt-4">Run Test</b-button>
       </b-col>
@@ -52,18 +64,62 @@ export default {
   data() {
     return {
       service_filter: "",
+      output_data: {},
       temp_filter: "",
       data: [],
       loaded: false,
     };
   },
   methods: {
+    add: function (data) {
+      // Handle undefined at top
+      this.$forceUpdate();
+      var output = JSON.parse(data);
+
+      // Need to handle deep nests - assume every parent is an object, except for the arrays
+
+      var item = output.item;
+      var parent = output.parent.replace("undefined.", "") || "";
+      var name = output.name;
+
+      var temp = JSON.parse(JSON.stringify(this.output_data))
+      this.output_data = ""
+      this.$forceUpdate()
+
+      const set = (obj, path, val) => {
+        const keys = path.split(".");
+        const lastKey = keys.pop();
+        const lastObj = keys.reduce((obj, key) => {
+          return (obj[key] = obj[key] || {});
+        }, obj);
+        lastObj[lastKey] = val;
+      };
+      var outtie = parent + "." + name;
+
+      set(temp, outtie, item);
+
+      if (temp[""] != undefined) {
+        var keys = Object.keys(temp[""]);
+        for (var i = 0; i < keys.length; i++) {
+          var next_key = keys[i];
+          if (temp[next_key] == undefined) {
+            temp[next_key] = temp[""][next_key];
+          }
+        }
+        delete temp[""];
+      }
+
+      this.output_data = temp
+
+      this.$forceUpdate();
+    },
+
     loadStructure: /* istanbul ignore next */ function () {
       var auth = this.$auth;
       Helper.apiCall("redis", "get_structure", auth)
         .then((res) => {
           this.data = res;
-          this.loaded = true
+          this.loaded = true;
         })
         .catch((e) => {
           this.$store.commit("updateError", e);
@@ -81,7 +137,7 @@ export default {
 </script>
 
 <style scoped>
-textarea{
+textarea {
   width: 100%;
   min-height: 400px;
 }
