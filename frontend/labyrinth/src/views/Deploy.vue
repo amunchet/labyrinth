8642eec621205e36c06d5828b475ad33f725754c
Chester Enright <cenright@altamontco.com>
2021-07-28 13:19:27 -0500

    refactor: refactored and fixed loading hosts

--- frontend/labyrinth/src/views/Deploy.vue
+++ frontend/labyrinth/src/views/Deploy.vue
@@ -3,18 +3,21 @@
     <b-row>
       <b-col>
         <h4>Deployment Files</h4>
-        These must be encrypted vault files.  Select the ones you need (for example, you might be deploying without SSH keys)
+        These must be encrypted vault files. Select the ones you need (for
+        example, you might be deploying without SSH keys)
         <hr />
 
         <b-row>
-          <b-col > SSH Key </b-col>
-        </b-row><b-row>
+          <b-col> SSH Key </b-col> </b-row
+        ><b-row>
           <b-col>
-            <b-select disabled v-if="files_list['ssh'] != undefined" :options="files_list['ssh']" />
+            <b-select
+              v-if="files_list['ssh'] != undefined"
+              :options="files_list['ssh']"
+            />
           </b-col>
           <b-col>
             <b-form-file
-              disabled
               class="float-left"
               v-model="ssh_key_file"
               placeholder="..."
@@ -26,14 +29,18 @@
             </b-button>
           </b-col>
         </b-row>
-        
+
         <b-row>
-          <b-col> Become Password </b-col>
-        </b-row><b-row>
+          <b-col> Become Password </b-col> </b-row
+        ><b-row>
           <b-col>
-            <b-select  v-if="files_list['become'] != undefined" :options="files_list['become']" v-model="selected_become" />
+            <b-select
+              v-if="files_list['become'] != undefined"
+              :options="files_list['become']"
+              v-model="selected_become"
+            />
           </b-col>
-          <b-col >
+          <b-col>
             <b-form-file
               v-model="file1"
               placeholder="..."
@@ -48,23 +55,23 @@
         </b-row>
         <b-row>
           <b-col>
-          <hr />
-          Become password needs to be in the following format: <br />
-          <code class="text-left">
-            ---<br />
-            ansible_ssh_pass: "XXXXXXX"
-          </code>
+            <hr />
+            Become password needs to be in the following format: <br />
+            <code class="text-left">
+              ---<br />
+              ansible_ssh_pass: "XXXXXXX"
+            </code>
           </b-col>
         </b-row>
         <hr />
-        
+
         <b-row>
-          
-          <b-col> Other Ansible Files (optional) </b-col>
-        </b-row><b-row>
+          <b-col> Other Ansible Files (optional) </b-col> </b-row
+        ><b-row>
           <b-col>
-            <b-select 
-            v-if="files_list['other'] != undefined" :options="files_list['other']"
+            <b-select
+              v-if="files_list['other'] != undefined"
+              :options="files_list['other']"
             />
           </b-col>
           <b-col>
@@ -88,53 +95,86 @@
         <hr />
         <div class="text-left">
           To create ansible vault files, use the following command:
-          <code>
-          ansible-vault create [FILENAME]
-          </code><br />
+          <code> ansible-vault create [FILENAME] </code><br />
           <i>New Vault Password: XXXXXXX</i>
-          
+
           [Edit file]
         </div>
       </b-col>
       <b-col>
         <div class="overflow-hidden">
-        <h2 v-if="!isTesting" class="float-left">Deploy
-        </h2>
-        <h2 class="float-left text-success" v-else>TESTING</h2>
-        <div class="float-right mt-1">
-          <b-button variant="success" v-if="!isTesting" @click="isTesting = !isTesting">Enter Test Mode</b-button>
-          <b-button variant="primary" v-else @click="isTesting = !isTesting">Enter Production Mode</b-button>
+          <h2 v-if="!isTesting" class="float-left">Deploy</h2>
+          <h2 class="float-left text-success" v-else>TESTING</h2>
+          <div class="float-right mt-1">
+            <b-button
+              variant="success"
+              v-if="!isTesting"
+              @click="isTesting = !isTesting"
+              >Enter Test Mode</b-button
+            >
+            <b-button variant="primary" v-else @click="isTesting = !isTesting"
+              >Enter Production Mode</b-button
+            >
+          </div>
         </div>
+        <div class="mb-4 mt-2" v-if="!isTesting">
+          Host: <b-select v-model="selected_host" :options="hosts" />
         </div>
-        <div class="mb-4 mt-2" v-if='!isTesting'>
-        Host: <b-select v-model="selected_host" :options="hosts" />
-        </div>
-        <div v-else class='mb-4 mt-2'>
-          Host: <br /><b>sampleclient</b> <br />(Backend ip is {{ip}})
+        <div v-else class="mb-4 mt-2">
+          Host: <br /><b>sampleclient - ({{sample_ip}})</b> <br />Backend ip is {{ ip }}
         </div>
         <h5 class="mt-2">Ansible playbook</h5>
-        <b-select class="mt-2 mb-2" v-if="files_list['ansible'] != undefined" :options="files_list['ansible']" v-model="selected_playbook" />
-        <b-textarea v-model="playbook_contents" v-if="loadings.playbook == undefined || loadings.playbook == 0"/>
+        <b-select
+          class="mt-2 mb-2"
+          v-if="files_list['ansible'] != undefined"
+          :options="files_list['ansible']"
+          v-model="selected_playbook"
+        />
+        <b-textarea
+          v-model="playbook_contents"
+          v-if="loadings.playbook == undefined || loadings.playbook == 0"
+        />
         <div v-else class="text-center">
-          <b-spinner class="ml-auto mr-auto mt-4" /></div>
-        <div class="overflow-hidden">
-        <b-button variant="success" class="mb-2 float-right" @click="savePlaybook()" v-if="loadings.save_playbook == undefined || loadings.save_playbook == 0">
-          <font-awesome-icon icon="save" size="1x" />
-        </b-button>
-        <div class="mb-2 float-right" v-else>
-          <b-spinner />
+          <b-spinner class="ml-auto mr-auto mt-4" />
         </div>
+        <div class="overflow-hidden">
+          <b-button
+            variant="success"
+            class="mb-2 float-right"
+            @click="savePlaybook()"
+            v-if="
+              loadings.save_playbook == undefined || loadings.save_playbook == 0
+            "
+          >
+            <font-awesome-icon icon="save" size="1x" />
+          </b-button>
+          <div class="mb-2 float-right" v-else>
+            <b-spinner />
+          </div>
         </div>
         <div>
-        Vault Password:
-        <b-input type="password" v-model="vault_password" />
+          Ansible username:
+          <b-input v-model="ansible_user" />
+        </div>
+
+        <div>
+          Vault Password:
+          <b-input type="password" v-model="vault_password" />
         </div>
         <hr />
 
-        <b-button v-if="isTesting" variant="success" @click="runPlaybook()">Deploy to sampleclient</b-button>
-        <b-button v-else variant="primary" @click="runPlaybook()">Deploy to hosts</b-button>
+        <b-button v-if="isTesting" variant="success" @click="runPlaybook()"
+          >Deploy to sampleclient</b-button
+        >
+        <b-button v-else variant="primary" @click="runPlaybook()"
+          >Deploy to hosts</b-button
+        >
         <br />
-        <div class="playbook_result" v-html="$sanitize(playbook_result)" v-if="playbook_result && playbook_loaded"></div>
+        <div
+          class="playbook_result"
+          v-html="$sanitize(playbook_result)"
+          v-if="playbook_result && playbook_loaded"
+        ></div>
         <b-spinner class="m-2" v-if="!playbook_loaded" />
       </b-col>
     </b-row>
@@ -151,6 +191,7 @@ export default {
       ssh_key_file: [],
 
       ip: "",
+      sample_ip: "",
 
       files_list: {},
 
@@ -158,6 +199,7 @@ export default {
       selected_host: "",
 
       vault_password: "",
+      ansible_user: "",
 
       selected_playbook: "",
       selected_become: "",
@@ -169,13 +211,13 @@ export default {
       loadings: {},
 
       isTesting: false,
-      options:[
+      options: [
         {
           text: "Test Mode",
           value: styles.green,
-        }
-      ]
-    }
+        },
+      ],
+    };
   },
   watch: {
     ssh_key_file: function (val) {
@@ -191,121 +233,143 @@ export default {
             this.$store.commit("updateError", e);
           });
       }
-      
-
     },
 
     //TODO: Finish other uploads
-    selected_playbook: function(val){
-      if(val != ""){
-        this.loadPlaybook(val)
+    selected_playbook: function (val) {
+      if (val != "") {
+        this.loadPlaybook(val);
       }
-    }
-
-
+    },
   },
   methods: {
-    loadPlaybook: /* istanbul ignore next */ function(){
-      var auth = this.$auth
-      var loadings = this.loadings
-      this.loadings["playbook"] = 1
-      Helper.apiCall("get_ansible_file", this.selected_playbook, auth).then(res=>{
-        this.playbook_contents = res
-        delete loadings.playbook 
-        this.$forceUpdate()
-      }).catch(e=>{
-        this.$store.commit('updateError', e)
-      })
+    loadPlaybook: /* istanbul ignore next */ function () {
+      var auth = this.$auth;
+      var loadings = this.loadings;
+      this.loadings["playbook"] = 1;
+      Helper.apiCall("get_ansible_file", this.selected_playbook, auth)
+        .then((res) => {
+          this.playbook_contents = res;
+          delete loadings.playbook;
+          this.$forceUpdate();
+        })
+        .catch((e) => {
+          this.$store.commit("updateError", e);
+        });
     },
 
-    savePlaybook: /* istanbul ignore next */ function(){
-
-      this.loadings["save_playbook"] = 1
-      this.$forceUpdate()
-      var auth = this.$auth
-      var formData = new FormData()
-      formData.append("data", this.playbook_contents)
-      Helper.apiPost("save_ansible_file/", this.selected_playbook, "", auth, formData).then(res=>{
-        this.$store.commit('updateError', res)
-        this.loadings["save_playbook"] = 0
-        this.loadPlaybook()
-      }).catch(e=>{
-        this.$store.commit('updateError', e)
-      })
+    savePlaybook: /* istanbul ignore next */ function () {
+      this.loadings["save_playbook"] = 1;
+      this.$forceUpdate();
+      var auth = this.$auth;
+      var formData = new FormData();
+      formData.append("data", this.playbook_contents);
+      Helper.apiPost(
+        "save_ansible_file/",
+        this.selected_playbook,
+        "",
+        auth,
+        formData
+      )
+        .then((res) => {
+          this.$store.commit("updateError", res);
+          this.loadings["save_playbook"] = 0;
+          this.loadPlaybook();
+        })
+        .catch((e) => {
+          this.$store.commit("updateError", e);
+        });
     },
 
-    runPlaybook: /* istanbul ignore next */
-    function(){
-      var auth = this.$auth
-      var formData = new FormData()
-      let host
-      if(this.isTesting){
-        host = "sampleclient"
-      }else{
-        host = this.selected_host
+    /* istanbul ignore next */
+    runPlaybook: function () {
+      var auth = this.$auth;
+      var formData = new FormData();
+      let host;
+      if (this.isTesting) {
+        host = "sampleclient";
+      } else {
+        host = this.selected_host;
       }
 
-      this.playbook_loaded = false
+      this.playbook_loaded = false;
       var data = {
         hosts: host,
         playbook: this.selected_playbook.replace(".yml", ""),
         vault_password: this.vault_password,
         become_file: this.selected_become.replace(".yml", ""),
-      }
-      formData.append("data", JSON.stringify(data))
+      };
+      formData.append("data", JSON.stringify(data));
 
-      Helper.apiPost("ansible_runner", "", "", auth, formData).then(res=>{
-        this.playbook_result = res
-        this.playbook_loaded = true
-      }).catch(e=>{
-        this.$store.commit('updateError', e)
-        this.playbook_loaded = true
-      })
+      Helper.apiPost("ansible_runner", "", "", auth, formData)
+        .then((res) => {
+          this.playbook_result = res;
+          this.playbook_loaded = true;
+        })
+        .catch((e) => {
+          this.$store.commit("updateError", e);
+          this.playbook_loaded = true;
+        });
     },
 
-    loadIP: /* istanbul ignore next */ function(){
-      var auth = this.$auth
-      Helper.apiCall("find_ip", "", auth).then(res=>{
-        this.ip = res
-      }).catch(e=>{
-        this.$store.commit('updateError', e)
-      })
+    loadIP: /* istanbul ignore next */ function () {
+      var auth = this.$auth;
+      Helper.apiCall("find_ip", "", auth)
+        .then((res) => {
+          this.ip = res;
+        })
+        .catch((e) => {
+          this.$store.commit("updateError", e);
+        });
+      Helper.apiCall("find_ip", "sampleclient", auth)
+        .then((res) => {
+          this.sample_ip = res;
+        })
+        .catch((e) => {
+          this.$store.commit("updateError", e);
+        });
     },
 
     loadFilesList: /* istanbul ignore next */ async function (type) {
       var auth = this.$auth;
       Helper.apiCall("uploads", type, auth)
         .then((res) => {
-          this.files_list[type]= res;
-          this.$forceUpdate()
+          this.files_list[type] = res;
+          this.$forceUpdate();
         })
         .catch((e) => {
           this.$store.commit("updateError", e);
         });
     },
-    loadHosts: /* istanbul ignore next */ function(){
-      var auth = this.$auth
-      Helper.apiCall("hosts", "", auth).then(res=>{
-        this.hosts = res
-      }).catch(e=>{
-        this.$store.commit('updateError', e)
-      })
-    }
+    loadHosts: /* istanbul ignore next */ function () {
+      var auth = this.$auth;
+      Helper.apiCall("hosts", "", auth)
+        .then((res) => {
+          this.hosts = res.map(x=>{
+            return{
+              text: x.ip,
+              value: x.ip
+            }
+          });
+        })
+        .catch((e) => {
+          this.$store.commit("updateError", e);
+        });
+    },
   },
-  
+
   mounted: /* istanbul ignore next */ async function () {
     try {
-
       await this.loadFilesList("ansible");
       this.loadFilesList("ssh");
       this.loadFilesList("become");
       this.loadFilesList("other");
       this.loadFilesList("telegraf");
       this.loadFilesList("totp");
-      
-      this.loadIP()
-      
-      this.loadHosts()
+
+      this.loadIP();
+
+      this.loadHosts();
     } catch (e) {
       this.$store.commit("updateError", e);
     }
@@ -315,13 +379,12 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/variables.scss";
 
-.playbook_result{
+.playbook_result {
   height: 400px;
   overflow-y: scroll;
   margin-top: 2rem;
   background-color: lightgrey;
   padding: 1rem;
-  
 }
 
 textarea {
@@ -336,11 +399,10 @@ textarea {
   text-align: left;
   margin-left: 2rem;
 }
-.cursor{
+.cursor {
   cursor: pointer;
 }
-.green{
+.green {
   color: $green;
 }
-
 </style>
