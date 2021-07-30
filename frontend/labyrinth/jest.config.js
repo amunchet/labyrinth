4b01ce1efd1b6d815f0986e3c46772a8a6f31126
Chester Enright <cenright@altamontco.com>
2021-07-30 17:42:41 -0500

    chore: added vue testing and coverage

--- frontend/labyrinth/jest.config.js
+++ frontend/labyrinth/jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
+    preset: "@vue/cli-plugin-unit-jest",
+    "collectCoverage": true,
+    "collectCoverageFrom": ["**/*.{js,vue}", "!**/node_modules/**"],
+    "coverageThreshold": {
+        "global" : {
+            "lines" : 90
+        }
+    }
 };
