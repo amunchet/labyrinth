be81381bebc8d8e33ece2d3afec346df001eaa46
Chester Enright <cenright@altamontco.com>
2021-07-29 20:20:38 -0500

    feat: wip - starting on alertmanager

--- frontend/labyrinth/src/views/Alerts.vue
+++ frontend/labyrinth/src/views/Alerts.vue
@@ -1,6 +1,10 @@
 <template>
   <b-container>
-    <h2>
+
+ <b-tabs content-class="mt-3">
+    <b-tab title="First" active>
+      <p>I'm the first tab</p>
+      <h2>
       Contact Information
     </h2>
     <h2>Options</h2>
@@ -12,5 +16,36 @@
         Grouping by hosts, services, summary, etc?
       </li>
     </ul>
-  </b-container>
+
+      
+      </b-tab>
+    <b-tab title="Second">
+      <p>I'm the second tab</p>
+      <iframe :src="frame_url.replace('api', 'alertmanager')" />
+      </b-tab>
+    <b-tab title="Disabled" disabled><p>I'm a disabled tab!</p></b-tab>
+  </b-tabs>
+
+      </b-container>
 </template>
+<script>
+import Helper from '@/helper'
+export default {
+  name: "Alerts",
+  data(){
+    return{
+      frame_url: "",
+    }
+  },
+  mounted: function(){
+    this.frame_url = Helper.getURL()
+  }
+}
+</script>
+<style lang="scss" scoped>
+iframe{
+  width: 100%;
+  min-height: 500px;
+  overflow-y: scroll;
+}
+</style>
