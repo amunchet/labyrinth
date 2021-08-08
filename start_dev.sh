1b5db3b43a6fc15e0d1db5e270ddd3f4e4f0581c
Chester Enright <amunchet@gmail.com>
2021-08-07 20:51:24 -0500

    fix(devel): corrections to allow starting on remote development

--- start_dev.sh
+++ start_dev.sh
@@ -2,5 +2,16 @@
 
 # TODO: Need to start the CVE docker stack as well
 
+# Need to create the SSL certs if they don't exist...
+
+echo "Creating Development SSL..."
+LEGODIR="nginx/.lego"
+mkdir $LEGODIR || true
+
+DOMAINNAME="development"
+
+openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=US/ST=IL/L=Chicago/O=Development/CN=Common" -keyout $LEGODIR/certificates/$DOMAINNAME.key -out $LEGODIR/certificates/$DOMAINNAME.crt
+
+
 echo "Starting development docker..."
 docker-compose -f docker-compose-development.yml up --build -d
