6ca256385be64443bb1a574ec66711e9efb07dca
Chester Enright <amunchet@gmail.com>
2021-08-07 21:11:22 -0500

    fix: got bash syntax backwards

--- start_dev.sh
+++ start_dev.sh
@@ -15,9 +15,9 @@ DOMAINNAME="development"
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=US/ST=IL/L=Chicago/O=Development/CN=Common" -keyout $LEGODIR/certificates/$DOMAINNAME.key -out $LEGODIR/certificates/$DOMAINNAME.crt
 
 if [ -z "${GITHUB}" ]; then
-    echo "[CI] Starting only CI dockers..."
-    docker-compose -f docker-compose-development.yml up --build -d frontend
-else
     echo "Starting development docker..."
     docker-compose -f docker-compose-development.yml up --build -d
+else
+    echo "[CI] Starting only CI dockers..."
+    docker-compose -f docker-compose-development.yml up --build -d frontend
 fi
