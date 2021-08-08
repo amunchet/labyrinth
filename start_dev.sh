4fe114b090338ef324582471ee22e8873abb94f1
Chester Enright <amunchet@gmail.com>
2021-08-07 21:04:30 -0500

    fix: correcting push and start_dev

--- start_dev.sh
+++ start_dev.sh
@@ -4,14 +4,20 @@
 
 # Need to create the SSL certs if they don't exist...
 
+echo "Touching nginx/.env..."
+touch nginx/.env || true
+
 echo "Creating Development SSL..."
 LEGODIR="nginx/.lego"
 mkdir $LEGODIR || true
 
 DOMAINNAME="development"
-
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=US/ST=IL/L=Chicago/O=Development/CN=Common" -keyout $LEGODIR/certificates/$DOMAINNAME.key -out $LEGODIR/certificates/$DOMAINNAME.crt
 
-
-echo "Starting development docker..."
-docker-compose -f docker-compose-development.yml up --build -d
+if [ -z "${GITHUB}" ]; then
+    echo "[CI] Starting only CI dockers..."
+    docker-compose -f docker-compose-development.yml up --build -d frontend
+else
+    echo "Starting development docker..."
+    docker-compose -f docker-compose-development.yml up --build -d
+fi
