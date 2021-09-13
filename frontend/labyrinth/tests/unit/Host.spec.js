adf9d3d044f98b24019c9316940fba2f1eacc89e
Chester Enright <amunchet@gmail.com>
2021-09-12 23:06:26 -0500

    test(ui): added dragging tests

--- frontend/labyrinth/tests/unit/Host.spec.js
+++ frontend/labyrinth/tests/unit/Host.spec.js
@@ -10,13 +10,13 @@ import Instance from "@/components/Host.vue";
 Vue.use(store);
 
 config.mocks["$auth"] = {
-  profile: {
-    name: "Test Name",
-    picture: "Test.jpg",
-  },
-  idToken: 1,
-  login: function () {},
-  getAccessToken: function () {},
+    profile: {
+        name: "Test Name",
+        picture: "Test.jpg",
+    },
+    idToken: 1,
+    login: function () { },
+    getAccessToken: function () { },
 };
 
 config.mocks["loaded"] = true;
@@ -24,76 +24,87 @@ config.mocks["loaded"] = true;
 let wrapper;
 
 beforeEach(() => {
-  wrapper = shallowMount(Instance, {
-    propsData: {
-      icon: "test",
-      passed_class: "test",
-      show_ports: "0",
-      host: "",
-      ip: "test.test.test",
-      services: [
-        {
-          name: "test",
+    wrapper = shallowMount(Instance, {
+        propsData: {
+            icon: "test",
+            passed_class: "test",
+            show_ports: "0",
+            host: "",
+            ip: "test.test.test",
+            services: [
+                {
+                    name: "test",
+                },
+            ],
+            cpu: "test",
+            mem: "test",
+            hd: "test",
+            options: [
+                "All",
+                "utopiany",
+                "rousingr",
+                "cunningh",
+                "papayawi",
+                "elegantc",
+                "tidyseri",
+                "quirkyco",
+            ],
+            onChange() {
+                //console.log('select changed')
+            },
         },
-      ],
-      cpu: "test",
-      mem: "test",
-      hd: "test",
-      options: [
-        "All",
-        "utopiany",
-        "rousingr",
-        "cunningh",
-        "papayawi",
-        "elegantc",
-        "tidyseri",
-        "quirkyco",
-      ],
-      onChange() {
-        //console.log('select changed')
-      },
-    },
-    store,
-    methods: {},
-    stubs: [
-      "font-awesome-icon",
-      "b-modal",
-      "b-button",
-      "b-select",
-      "b-input",
-      "b-row",
-      "b-col",
-      "b-table",
-      "b-tab",
-      "b-tabs",
-      "b-spinner",
-      "b-container",
-      "b-textarea",
-      "b-avatar",
-      "b-form-file",
-    ],
-  });
+        store,
+        methods: {},
+        stubs: [
+            "font-awesome-icon",
+            "b-modal",
+            "b-button",
+            "b-select",
+            "b-input",
+            "b-row",
+            "b-col",
+            "b-table",
+            "b-tab",
+            "b-tabs",
+            "b-spinner",
+            "b-container",
+            "b-textarea",
+            "b-avatar",
+            "b-form-file",
+        ],
+    });
 });
 
 afterEach(() => {
-  wrapper.destroy();
+    wrapper.destroy();
 });
 
 describe("Host.vue", () => {
-  test("is a Vue instance", () => {
-    expect(wrapper.isVueInstance).toBeTruthy();
-  });
+    test("is a Vue instance", () => {
+        expect(wrapper.isVueInstance).toBeTruthy();
+    });
 
-  test("determineClass", () => {
-    var service = {
-      state: -1,
-    };
-    expect(wrapper.vm.determineClass(service));
-    service.state = false;
+    test("determineClass", () => {
+        var service = {
+            state: -1,
+        };
+        expect(wrapper.vm.determineClass(service));
+        service.state = false;
 
-    expect(wrapper.vm.determineClass(service));
-    service.state = 71;
+        expect(wrapper.vm.determineClass(service));
+        service.state = 71;
 
-    expect(wrapper.vm.determineClass(service));
-  });
+        expect(wrapper.vm.determineClass(service));
+    });
+
+    test("drag_start", () => {
+        wrapper.vm.drag_start("TESTIP")
+        expect(wrapper.vm.$data.drag_class).toEqual("dragging")
+        expect(wrapper.vm.$data.dragging_ip).toEqual("TESTIP")
+    })
+    test("drag_end", () => {
+        wrapper.vm.drag_end()
+        expect(wrapper.vm.$data.drag_class).toEqual("")
+        expect(wrapper.vm.$data.dragging_ip).toEqual("")
+    })
 });
