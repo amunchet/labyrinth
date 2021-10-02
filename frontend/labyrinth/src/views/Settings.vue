5372d9e08a9b6b1d59f50629288aef6d698efc63
Chester Enright <amunchet@gmail.com>
2021-10-02 16:59:15 -0500

    feat: wip - icons in settings

---
+++ frontend/labyrinth/src/views/Settings.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="text-left ml-4">
+    <b-row>
+      <b-col class="sub">
+        <h3>Icons</h3>
+        <div class="flex">
+          <div v-for="(item, idx) in icons" v-bind:key="idx" class="box">
+            <div class="overflow-hidden">
+              <font-awesome-icon icon="times" size="1x" class="cursor float-right" 
+              @click="deleteIcon()"
+              />
+            </div>
+            <img :src="'/icons/' + item + '.svg'" :alt="item + ' icon'" />
+            <div class="box_text">
+              {{ item }}
+            </div>
+          </div>
+          <div class="box pt-4 bg-success cursor text-white">
+          <div class="mt-4 cursor" >
+            + Upload New Icon (TODO)
+            </div>
+            </div>
+        </div>
+      </b-col>
+
+      <b-col class="sub">
+        <h3>Colors/Themes</h3>
+        TODO: Coming Soon
+      </b-col>
+    </b-row>
+  </div>
+</template>
+<script>
+import Helper from "@/helper";
+export default {
+  name: "Settings",
+  data() {
+    return {
+      icons: [],
+    };
+  },
+  methods: {
+    loadIcons: /* istanbul ignore next */ function () {
+      var auth = this.$auth;
+      Helper.apiCall("icons", "", auth)
+        .then((res) => {
+          this.icons = res;
+        })
+        .catch((e) => {
+          this.$store.commit("updateError", e);
+        });
+    },
+    deleteIcon: /* istanbul ignore next */ function (){
+      alert("TODO: Delete icon")
+    },
+  },
+  mounted: function () {
+    this.loadIcons();
+  },
+};
+</script>
+<style lang="scss" scoped>
+.sub {
+  border-right: 1px solid lightgrey;
+  min-height: 400px;
+  text-align: center;
+  padding-top: 1rem;
+}
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+}
+.box {
+  border: 1px solid lightgrey;
+  width: 25%;
+  margin: 1rem;
+  padding: 1rem;
+  text-align: center;
+}
+.box_text {
+  margin-top: 0.5rem;
+}
+.cursor {
+  cursor: pointer;
+}
+</style>
