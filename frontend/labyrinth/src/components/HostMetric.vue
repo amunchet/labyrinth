95a1f773a656c0b82eb4396ced447690a3f99dc8
Chester Enright <amunchet@gmail.com>
2021-11-06 18:10:30 -0500

    fix: service detail small fixes Reversed table and graph to reflect stale metrics

--- frontend/labyrinth/src/components/HostMetric.vue
+++ frontend/labyrinth/src/components/HostMetric.vue
@@ -2,7 +2,7 @@
   <b-modal id="service_detail" title="Service Details" size="xl">
     <line-chart v-if="display" height="100px" :chart-data="datacollection"></line-chart>
 
-    <b-table :items="result" v-if="!loading" :fields="['name', 'tags', 'fields', 'timestamp', 'judgement']">
+    <b-table :items="result_backwards" v-if="!loading" :fields="['name', 'tags', 'fields', 'timestamp', 'judgement']">
       <template v-slot:cell(timestamp)="row">
         {{formatDate(row.item.timestamp * 1000)}} {{formatDate(row.item.timestamp * 1000, true)}}
       </template>
@@ -27,6 +27,7 @@ export default {
       datacollection: null,
       display: false,
       result: [],
+      result_backwards: [],
       loading: false,
     };
   },
@@ -47,6 +48,7 @@ export default {
         )
           .then((res) => {
             this.result = res;
+            this.result_backwards = JSON.parse(JSON.stringify(res)).reverse()
             this.loading = false;
             this.datacollection = {
               labels: this.result.map((x) => this.formatDate(x["timestamp"] * 1000, true)),
@@ -54,12 +56,18 @@ export default {
                 {
                   label: "Success",
                   backgroundColor: "green",
-                  data: this.result.map((x) => x["judgement"] * 1),
+                  data: this.result.map((x) => {
+                    if(x["judgement"] == true){
+                      return 1
+                    }else{
+                      return 0
+                    }
+                  }),
                 },
                 {
                   label: "Failure",
                   backgroundColor: "red",
-                  data: this.result.map(x=>(x["judgement"] == false) * 1)
+                  data: this.result.map(x=>(x["judgement"] != true) * 1)
                 }
               ],
             };
