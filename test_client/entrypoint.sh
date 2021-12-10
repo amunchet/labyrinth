3c527d1bd7d881b02af4584ea6e99e9c2e697a62
Chester Enright <amunchet@gmail.com>
2021-12-10 00:17:34 -0600

    chore: wip - added sample key to test client docker

--- test_client/entrypoint.sh
+++ test_client/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Copying over sample SSH key to authorized..."
+mkdir /root/.ssh || true
+cat /src/sample_key.pub > /root/.ssh/authorized_keys
+
 echo "Starting SSH server..."
 service ssh start
 
