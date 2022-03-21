155996f8282ee4d67efa3f0839598c444ac5c0ba
Chester Enright <amunchet@gmail.com>
2022-03-21 01:03:54 -0500

    fix(run_tests): corrected to include user arguments

--- backend/run_tests.sh
+++ backend/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 CODECOV="95"
 DOCKER_PROD_NAME="labyrinth_backend_1"
-
+ARGS="$@"
 # Running Python unit tests and coverage
 echo "Running Pytest..."
 # -x is stop on first failure
@@ -14,5 +14,5 @@ if [ $? -eq 127 ]; then
 
 else
     echo "Running tests in local"
-    docker exec $DOCKER_PROD_NAME sh -c "cd /src && PYTHONPATH=/src pytest --cov=/src --cov-report term-missing -vvvv --cov-fail-under=95 --cov-report=html $1 . "
+    docker exec $DOCKER_PROD_NAME sh -c "cd /src && PYTHONPATH=/src pytest --cov=/src --cov-report term-missing -vvvv --cov-fail-under=95 --cov-report=html $ARGS . "
 fi
