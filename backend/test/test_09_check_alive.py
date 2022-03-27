d9ad4112363f76e2e757f61ff1d91c1222ff41f0
Chester Enright <amunchet@gmail.com>
2022-03-27 01:40:47 -0500

    test: wip - starting on check alive

---
+++ backend/test/test_09_check_alive.py
@@ -0,0 +1,14 @@
+"""
+Check Alive tests
+    - TODO: Look at Nagios and see what options/configurations are done there
+"""
+
+def test_alive_ping():
+    """
+    Detects alive from ping
+    """
+
+def test_alive_port():
+    """
+    Detects Alive from a port connection
+    """
