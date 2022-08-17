fd8c446c46bcb82184813430634fdf92eeb5ad80
Chester Enright <amunchet@gmail.com>
2022-08-17 15:26:26 -0500

    test: wip - starting on maps tests

---
+++ backend/test/test_11_maps.py
@@ -0,0 +1,37 @@
+
+# Maps
+def test_list_maps():
+    """
+    Lists out the maps
+    """
+
+def test_create_edit_delete_map():
+    """
+    Creates or edits a map
+        - This will have all images used in the map (local and remote (maybe?))
+        - Plugins (i.e. hosts, services, etc) and how they're displayed (compact, full, etc.)
+    """
+    # Create
+
+    # Edit
+
+    # Delete
+
+
+# Map Images
+def test_list_map_images():
+    """
+    Lists all available maps
+    """
+
+def test_upload_map_image():
+    """
+    Tests uploading a map
+    """
+
+def test_delete_map_image():
+    """
+    Tests Deleting a map
+        - Probably want to check if the map is in use
+    """
+
