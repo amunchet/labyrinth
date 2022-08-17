8f481b28d0cbbdb5b84f2631dacba86f29bb0784
Chester Enright <amunchet@gmail.com>
2022-08-17 17:17:25 -0500

    test: working custom dashboard tests

--- backend/test/test_10_custom_dashboard.py
+++ backend/test/test_10_custom_dashboard.py
@@ -22,6 +22,12 @@ def tearDown():
     if os.path.exists("/src/uploads/images"):
         shutil.rmtree("/src/uploads/images")
 
+    if os.path.exists("/tmp/floorplan.jpg"):
+        os.remove("/tmp/floorplan.jpg")
+
+    if os.path.exists("/tmp/broken.jpg"):
+        os.remove("/tmp/broken.jpg")
+
 
     serve.mongo_client["labyrinth"]["dashboards"].delete_many({"name" : "TESTTEST"})
 
@@ -29,6 +35,13 @@ def tearDown():
 def setup():
     """Sets up"""
     tearDown()
+
+    floorplan = "/src/test/images/floorplan.jpg"
+    broken ="/src/test/images/broken.jpg" 
+
+    shutil.copy(floorplan, "/tmp/floorplan.jpg")
+    shutil.copy(broken, "/tmp/broken.jpg")
+
 
     yield "Setting up"
     tearDown()
@@ -46,7 +59,7 @@ def test_create_edit_custom_dashboard(setup):
     a = serve.mongo_client["labyrinth"]["dashboards"].find({"name" : "TESTTEST"})
     assert not list(a) 
 
-    a = unwrap(serve.test_create_edit_custom_dashboard)(data=SAMPLE_DASHBOARD)
+    a = unwrap(serve.create_edit_custom_dashboard)("TESTTEST", data=SAMPLE_DASHBOARD)
     assert a[1] == 200
 
     a = serve.mongo_client["labyrinth"]["dashboards"].find({"name" : "TESTTEST"})
@@ -56,25 +69,35 @@ def test_create_edit_custom_dashboard(setup):
     temp = deepcopy(SAMPLE_DASHBOARD)
     temp["hosts"][0]["display"] = "vertical"
 
-    a = unwrap(serve.test_create_edit_custom_dashboard)(data=SAMPLE_DASHBOARD)
+    a = unwrap(serve.create_edit_custom_dashboard)("TESTTEST", data=temp)
     assert a[1] == 200
     
-    a = serve.mongo_client["labyrinth"]["dashboards"].find({"name" : "TESTTEST"})
-    assert len(list(a)) == 1
-    assert list(a)[0]["hosts"][0]["display"] == "vertical"
+    a = list(serve.mongo_client["labyrinth"]["dashboards"].find({"name" : "TESTTEST"}))
+    assert len(a) == 1
+    print(a)
+    assert a[0]["hosts"][0]["display"] == "vertical"
 
 def test_list_and_delete_custom_dashboards(setup):
     """
     Lists all custom dashboards
     """
-    a = unwrap(serve.test_list_custom_dashboards)()
-    assert a[1] == 200
-    assert [x for x in json.loads(a[0]) if "name" in x and x["name"] == "TESTTEST"] == []
+    a = unwrap(serve.list_custom_dashboards)()
+    assert a[1] == 404
 
     test_create_edit_custom_dashboard(setup)
 
-    a = unwrap(serve.test_list_custom_dashboards)()
+    a = unwrap(serve.list_custom_dashboards)()
     assert a[1] == 200
+    b = [x for x in json.loads(a[0]) if "name" in x and x["name"] == "TESTTEST"] 
+    assert len(b) == 1
+
+    a = unwrap(serve.list_custom_dashboards)("TESSDFSDFS")
+    assert a[1] == 404
+
+    a = unwrap(serve.list_custom_dashboards)("TESTTEST")
+    assert a[1] == 200
+    assert json.loads(a[0])
+
     b = [x for x in json.loads(a[0]) if "name" in x and x["name"] == "TESTTEST"] 
     assert len(b) == 1
 
@@ -82,10 +105,8 @@ def test_list_and_delete_custom_dashboards(setup):
     a = unwrap(serve.delete_custom_dashboard)("TESTTEST")
     assert a[1] == 200
     
-    a = unwrap(serve.test_list_custom_dashboards)()
-    assert a[1] == 200
-    b = [x for x in json.loads(a[0]) if "name" in x and x["name"] == "TESTTEST"] 
-    assert len(b) == 0
+    a = unwrap(serve.list_custom_dashboards)()
+    assert a[1] == 404
 
 # Images
 def test_image_upload(setup):
@@ -94,23 +115,22 @@ def test_image_upload(setup):
         - This is separate from ansible upload
         - Main images and sub images
     """
-    with open("/src/test/images/floorplan.jpg") as f:
-        temp_file = f.read()
-    
-    with open("/src/test/images/broken.jpg") as f:
-        broken_file = f.read()
     
     assert not os.path.exists("/src/uploads/images")
 
-    a = unwrap(serve.custom_dashboard_image_upload)(data=broken_file, filename="floorplan.jpg")
-    assert a[1] == 484
-
-    a = unwrap(serve.custom_dashboard_image_upload)(data=temp_file, filename="floorplan.jpg")
+    a = unwrap(serve.custom_dashboard_image_upload)(override="floorplan.jpg", filename="floorplan.jpg")
     assert a[1] == 200
+
+    a = unwrap(serve.custom_dashboard_image_upload)(override="broken.jpg", filename="floorplan.jpg")
+    assert a[1] == 484
     
     assert os.path.exists("/src/uploads/images/floorplan.jpg")
 
     # Deletes an uploaded image
+
+    a = unwrap(serve.custom_dashboard_delete_image)("../../test/images/broken.jpg")
+    assert a[1] == 404
+
     a = unwrap(serve.custom_dashboard_delete_image)("floorplan.jpg")
     assert a[1] == 200
     
@@ -124,10 +144,8 @@ def test_images_list(setup):
     assert a[1] == 200
     assert json.loads(a[0]) == []
 
-    with open("/src/test/images/floorplan.jpg") as f:
-        temp_file = f.read()
     
-    a = unwrap(serve.custom_dashboard_image_upload)(data=temp_file, filename="floorplan.jpg")
+    a = unwrap(serve.custom_dashboard_image_upload)(override="floorplan.jpg", filename="floorplan.jpg")
     assert a[1] == 200
 
     a = unwrap(serve.custom_dashboard_list_images)()
