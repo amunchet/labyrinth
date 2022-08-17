ec7d2d963d877e963f6ccf2c688bc4f34f15d9bb
Chester Enright <amunchet@gmail.com>
2022-08-17 16:25:51 -0500

    chore: updates to custom dashboard tests

--- backend/test/test_10_custom_dashboard.py
+++ backend/test/test_10_custom_dashboard.py
@@ -7,6 +7,7 @@ import shutil
 import pytest
 import serve
 
+from copy import deepcopy
 
 from common.test import unwrap
 
@@ -15,17 +16,19 @@ with open("test/sample_custom_dashboard.json") as f:
 
 def tearDown():
     """Tear down"""
-    serve.mongo_client["labyrinth"]["dashboards"].delete_many({"name" : "TESTTEST"})
 
-    # TODO: Remove any extra images created
+    # Remove the images folder in uploads
+
+    if os.path.exists("/src/uploads/images"):
+        shutil.rmtree("/src/uploads/images")
+
+
+    serve.mongo_client["labyrinth"]["dashboards"].delete_many({"name" : "TESTTEST"})
 
 @pytest.fixture
 def setup():
     """Sets up"""
     tearDown()
-
-    # TODO: Create some image files
-    # TODO: Also some invalid
 
     yield "Setting up"
     tearDown()
@@ -47,12 +50,20 @@ def test_create_edit_custom_dashboard(setup):
     assert a[1] == 200
 
     a = serve.mongo_client["labyrinth"]["dashboards"].find({"name" : "TESTTEST"})
+    assert len(list(a)) == 1
+
+    # Check that we edit on the same name, not create new
+    temp = deepcopy(SAMPLE_DASHBOARD)
+    temp["hosts"][0]["display"] = "vertical"
+
+    a = unwrap(serve.test_create_edit_custom_dashboard)(data=SAMPLE_DASHBOARD)
     assert a[1] == 200
+    
+    a = serve.mongo_client["labyrinth"]["dashboards"].find({"name" : "TESTTEST"})
+    assert len(list(a)) == 1
+    assert list(a)[0]["hosts"][0]["display"] == "vertical"
 
-    # TODO: Check that we edit on the same name, not create new
-
-
-def test_list_custom_dashboards(setup):
+def test_list_and_delete_custom_dashboards(setup):
     """
     Lists all custom dashboards
     """
@@ -67,13 +78,14 @@ def test_list_custom_dashboards(setup):
     b = [x for x in json.loads(a[0]) if "name" in x and x["name"] == "TESTTEST"] 
     assert len(b) == 1
 
-
-
-def test_delete_custom_dashboard(setup):
-    """
-    Deletes a custom dashboard
-    """
-
+    # Deletes a custom dashboard
+    a = unwrap(serve.delete_custom_dashboard)("TESTTEST")
+    assert a[1] == 200
+    
+    a = unwrap(serve.test_list_custom_dashboards)()
+    assert a[1] == 200
+    b = [x for x in json.loads(a[0]) if "name" in x and x["name"] == "TESTTEST"] 
+    assert len(b) == 0
 
 # Images
 def test_image_upload(setup):
@@ -82,20 +94,48 @@ def test_image_upload(setup):
         - This is separate from ansible upload
         - Main images and sub images
     """
+    with open("/src/test/images/floorplan.jpg") as f:
+        temp_file = f.read()
+    
+    with open("/src/test/images/broken.jpg") as f:
+        broken_file = f.read()
+    
+    assert not os.path.exists("/src/uploads/images")
 
-def test_image_delete(setup):
-    """
-    Deletes an uploaded image
-        - Probably want to check if it's in a custom dashboard
-    """
+    a = unwrap(serve.custom_dashboard_image_upload)(data=broken_file, filename="floorplan.jpg")
+    assert a[1] == 484
 
-def test_image_rotate(setup):
-    """
-    Rotates an image
-    """
+    a = unwrap(serve.custom_dashboard_image_upload)(data=temp_file, filename="floorplan.jpg")
+    assert a[1] == 200
+    
+    assert os.path.exists("/src/uploads/images/floorplan.jpg")
+
+    # Deletes an uploaded image
+    a = unwrap(serve.custom_dashboard_delete_image)("floorplan.jpg")
+    assert a[1] == 200
+    
+    assert not os.path.exists("/src/uploads/images/floorplan.jpg")
 
 def test_images_list(setup):
     """
     Lists all uploaded images
     """
+    a = unwrap(serve.custom_dashboard_list_images)()
+    assert a[1] == 200
+    assert json.loads(a[0]) == []
+
+    with open("/src/test/images/floorplan.jpg") as f:
+        temp_file = f.read()
+    
+    a = unwrap(serve.custom_dashboard_image_upload)(data=temp_file, filename="floorplan.jpg")
+    assert a[1] == 200
+
+    a = unwrap(serve.custom_dashboard_list_images)()
+    assert a[1] == 200
+    assert json.loads(a[0]) == ["floorplan.jpg"]
+
+
+    
+
+
 
