3184fc0d101755b0c381006a5bb480b7f03454e1
Chester Enright <amunchet@gmail.com>
2022-08-30 09:31:34 -0500

    chore: added additional indexes

--- backend/serve.py
+++ backend/serve.py
@@ -1195,9 +1195,12 @@ def index_helper():
         [("timestamp", pymongo.DESCENDING)]
     )
     mongo_client["labyrinth"]["metrics"].create_index("name")
+    mongo_client["labyrinth"]["metrics"].create_index("tags")
     mongo_client["labyrinth"]["metrics"].create_index("tags.mac")
     mongo_client["labyrinth"]["metrics"].create_index("tags.ip")
+    mongo_client["labyrinth"]["metrics"].create_index("timestamp")
     mongo_client["labyrinth"]["services"].create_index("name")
+    mongo_client["labyrinth"]["services"].create_index("display_name")
     mongo_client["labyrinth"]["hosts"].create_index("ip")
     mongo_client["labyrinth"]["hosts"].create_index("mac")
     mongo_client["labyrinth"]["hosts"].create_index("subnet")
