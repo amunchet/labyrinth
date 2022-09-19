c3764436b840ab885e48703db8b9805d5e19e0ec
amunchet <amunchet@gmail.com>
2022-09-19 18:16:49 -0500

    chore: updated crons to use proper env

--- cron/run.sh
+++ cron/run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 echo "Starting finder..."
 cd /src/ 
-MONGO_HOST=mongo REDIS_HOST=redis MONGO_USERNAME=root MONGO_PASSWORD=temp python3 finder.py 2>&1 
+if [ -f .env ]; then
+	set -a;
+	source .env;
+	set +a;
+fi
+# MONGO_HOST=mongo REDIS_HOST=redis MONGO_USERNAME=root MONGO_PASSWORD=temp python3 finder.py 2>&1 
+python3 finder.py 2>&1 
 echo "Done."
