352ddb71bc2852bcbb0de76a77b30b61822b98cb
Chester Enright <amunchet@gmail.com>
2022-10-14 13:17:22 -0500

    chore: auto build vue app on production start

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -7,6 +7,12 @@ cd /src/labyrinth && npm install
 echo "Runing npm upgrade..."
 cd /src/labyrinth && npm upgrade
 
-echo "Starting..."
-# cd /src/ && npm run serve
-cd /src/ && vue ui -D -H 0.0.0.0
+
+if [[ -z "{$TESTBED}" ]]; then
+    echo "Building vue app..."
+    cd /src/labyrinth/ && npm run build
+else
+    echo "Starting devel server..."
+    # cd /src/ && npm run serve
+    cd /src/ && vue ui -D -H 0.0.0.0
+fi
